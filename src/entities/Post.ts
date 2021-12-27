b9f3b6801cc13f662a3eea1a7d01e23e964f1331
gavindang2911 <hoaidang2999@gmail.com>
2021-12-26 19:46:24 -0800

    Added entities for postgres

---
+++ src/entities/Post.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Post extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    title!: string
+
+    @Column()
+    text!: string
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @CreateDateColumn()
+    updatedAt: Date
+}
