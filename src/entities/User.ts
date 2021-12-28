614d6edfe2e6c84f18b69cd32d3619eb325aa28c
gavindang2911 <hoaidang2999@gmail.com>
2021-12-27 21:00:57 -0800

    added User resolver for graphql

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,22 +1,30 @@
+import { Field, ID, ObjectType } from 'type-graphql'
 import {BaseEntity, Column,CreateDateColumn, Entity, PrimaryGeneratedColumn} from 'typeorm'
 
+
+@ObjectType()
 @Entity() // db table
 export class User extends BaseEntity {
+    @Field(_type => ID)
     @PrimaryGeneratedColumn()
     id!: number
 
+    @Field()
     @Column({unique: true})
     username!: string
 
+    @Field()
     @Column({unique: true})
     email!: string
 
     @Column()
     password!: string
 
+    @Field()
     @CreateDateColumn()
     createdAt: Date
 
+    @Field()
     @CreateDateColumn()
     updatedAt: Date
 }
