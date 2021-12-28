fe9c0b26fdf6f470d6fd22d1b1eda24f388ed202
gavindang2911 <hoaidang2999@gmail.com>
2021-12-27 18:48:06 -0800

    working on resolver

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,42 @@
 require('dotenv').config();
 import 'reflect-metadata';
 import express from 'express';
-import {createConnection} from 'typeorm'
+import { createConnection } from 'typeorm';
 import { User } from './entities/User';
 import { Post } from './entities/Post';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
 const main = async () => {
-    await createConnection({
-        type: 'postgres',
-        database: 'reddit',
-        username: process.env.DB_USERNAME_DEV,
-        password: process.env.DB_PASSWORD_DEV,
-        logging: true,
-        synchronize: true,
-        entities: [User, Post]
-    })
+  await createConnection({
+    type: 'postgres',
+    database: 'reddit',
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    logging: true,
+    synchronize: true,
+    entities: [User, Post],
+  });
 
-    const app = express();
+  const app = express();
 
-    app.listen(4000, () => console.log(`Server started on port 4000`));
-}
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [HelloResolver], validate: false }),
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
 
-main().catch(error => console.log(error));
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () =>
+    console.log(
+      `Server started on port ${PORT}, graphQL started on port ${apolloServer.graphqlPath}`
+    )
+  );
+};
+
+main().catch((error) => console.log(error));
