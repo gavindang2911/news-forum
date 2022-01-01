8463ec2429ee06bbf9380ba3499512e420d36e1d
gavindang2911 <hoaidang2999@gmail.com>
2021-12-31 18:22:47 -0800

    refactor register mutation, adding validator for email and password field

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -2,31 +2,38 @@ import { User } from '../entities/User';
 import { Arg, Mutation, Resolver } from 'type-graphql';
 import argon2 from 'argon2';
 import { UserMutationResponse } from '../types/UserMutationResponse';
+import { RegisterInput } from '../types/RegisterInput';
+import { validateRegisterInput } from '../utils/validateRegisterInput';
 
 @Resolver()
 export class UserResolver {
   @Mutation((_returns) => UserMutationResponse, { nullable: true })
   async register(
-    @Arg('email') email: string,
-    @Arg('username') username: string,
-    @Arg('password') password: string
+    @Arg('registerInput') registerInput: RegisterInput
   ): Promise<UserMutationResponse> {
+    const validateRegisterInputErrors = validateRegisterInput(registerInput);
+    if (validateRegisterInputErrors != null)
+      return { code: 400, success: false, ...validateRegisterInputErrors };
+
     try {
+      const { username, email, password } = registerInput;
       const existingUser = await User.findOne({
         where: [{ username }, { email }],
       });
       if (existingUser)
-      return {
-        code: 400,
-        success: false,
-        message: 'Duplicated username or email',
-        errors: [
-          {
-            field: existingUser.username === username ? 'username' : 'email',
-            message: 'Username or email already taken'
-          }
-        ]
-      };
+        return {
+          code: 400,
+          success: false,
+          message: 'Duplicated username or email',
+          errors: [
+            {
+              field: existingUser.username === username ? 'username' : 'email',
+              message: `${
+                existingUser.username === username ? 'Username' : 'Email'
+              } already taken`,
+            },
+          ],
+        };
 
       const hashedPassword = await argon2.hash(password);
       const newUser = User.create({
@@ -40,7 +47,7 @@ export class UserResolver {
         success: true,
         message: 'User registration successful',
         user: await User.save(newUser),
-      }
+      };
     } catch (error) {
       console.log(error);
       return {
