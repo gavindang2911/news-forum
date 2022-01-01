c111bb36aa563a8b94e8498b8d5b0713e5b3b223
gavindang2911 <hoaidang2999@gmail.com>
2021-12-31 18:48:51 -0800

    Added login mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -4,10 +4,11 @@ import argon2 from 'argon2';
 import { UserMutationResponse } from '../types/UserMutationResponse';
 import { RegisterInput } from '../types/RegisterInput';
 import { validateRegisterInput } from '../utils/validateRegisterInput';
+import { LoginInput } from '../types/LoginInput';
 
 @Resolver()
 export class UserResolver {
-  @Mutation((_returns) => UserMutationResponse, { nullable: true })
+  @Mutation((_return) => UserMutationResponse)
   async register(
     @Arg('registerInput') registerInput: RegisterInput
   ): Promise<UserMutationResponse> {
@@ -56,5 +57,62 @@ export class UserResolver {
         message: `Internal server error ${error.message}`,
       };
     }
+  }
+
+  @Mutation((_return) => UserMutationResponse)
+  async login(
+    @Arg('loginInput') { usernameOrEmail, password }: LoginInput
+  ): Promise<UserMutationResponse> {
+    try {
+      const existingUser = await User.findOne(
+        usernameOrEmail.includes('@')
+          ? { email: usernameOrEmail}
+          : { username: usernameOrEmail}
+      );
+
+      if (!existingUser)
+      return {
+        code: 400,
+        success: false,
+        message: 'User not found',
+        errors: [
+          {
+            field: 'usernameOrEmail' ,
+            message: 'Username or email incorrect',
+          },
+        ],
+      };
+
+      const passwordValid = await argon2.verify(existingUser.password, password);
+      if (!passwordValid)
+      return {
+        code: 400,
+        success: false,
+        message: 'Wrong password',
+        errors: [
+          {
+            field: 'password' ,
+            message: 'Wrong password',
+          },
+        ],
+      }
+
+      return {
+        code: 200,
+        success: true,
+        message: 'Login successfully',
+        user: existingUser,
+      }
+    } catch (error) {
+      console.log(error);
+      return {
+        code: 500,
+        success: false,
+        message: `Internal server error ${error.message}`,
+      };
+    }
+
+
+
   }
 }
