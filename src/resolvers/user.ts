9574592acf2aac81106f677a7544fb821afff164
gavindang2911 <hoaidang2999@gmail.com>
2022-01-05 23:59:08 -0800

    Working on change password mutation in server side

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -177,4 +177,92 @@ export class UserResolver {
 
 		return true
 	}
+
+  @Mutation(_return => UserMutationResponse)
+	async changePassword(
+		@Arg('token') token: string,
+		@Arg('userId') userId: string,
+		@Arg('changePasswordInput') changePasswordInput: ChangePasswordInput,
+		@Ctx() { req }: Context
+	): Promise<UserMutationResponse> {
+		if (changePasswordInput.newPassword.length <= 2) {
+			return {
+				code: 400,
+				success: false,
+				message: 'Invalid password',
+				errors: [
+					{ field: 'newPassword', message: 'Length must be greater than 2' }
+				]
+			}
+		}
+
+		try {
+			const resetPasswordTokenRecord = await TokenModel.findOne({ userId })
+			if (!resetPasswordTokenRecord) {
+				return {
+					code: 400,
+					success: false,
+					message: 'Invalid or expired password reset token',
+					errors: [
+						{
+							field: 'token',
+							message: 'Invalid or expired password reset token'
+						}
+					]
+				}
+			}
+
+			const resetPasswordTokenValid = argon2.verify(
+				resetPasswordTokenRecord.token,
+				token
+			)
+
+			if (!resetPasswordTokenValid) {
+				return {
+					code: 400,
+					success: false,
+					message: 'Invalid or expired password reset token',
+					errors: [
+						{
+							field: 'token',
+							message: 'Invalid or expired password reset token'
+						}
+					]
+				}
+			}
+
+			const userIdNum = parseInt(userId)
+			const user = await User.findOne(userIdNum)
+
+			if (!user) {
+				return {
+					code: 400,
+					success: false,
+					message: 'User no longer exists',
+					errors: [{ field: 'token', message: 'User no longer exists' }]
+				}
+			}
+
+			const updatedPassword = await argon2.hash(changePasswordInput.newPassword)
+			await User.update({ id: userIdNum }, { password: updatedPassword })
+
+			await resetPasswordTokenRecord.deleteOne()
+
+			req.session.userId = user.id
+
+			return {
+				code: 200,
+				success: true,
+				message: 'User password reset successfully',
+				user
+			}
+		} catch (error) {
+			console.log(error)
+			return {
+				code: 500,
+				success: false,
+				message: `Internal server error ${error.message}`
+			}
+		}
+	}
 }
