7a1c52aa93a602ce3e60ad22ca5e48b15cf68e2f
gavindang2911 <hoaidang2999@gmail.com>
2022-01-06 01:27:16 -0800

    refactor forgot password page

--- client/src/pages/forgot-password.tsx
+++ client/src/pages/forgot-password.tsx
@@ -1,52 +1,65 @@
-import { Box, Button, Flex, Link } from '@chakra-ui/react';
+import { Box, Button, Flex, Link, Spinner } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
 import InputField from '../components/InputField';
 import Wrapper from '../components/Wrapper';
-import NextLink from 'next/link'
-import { ForgotPasswordInput, useForgotPasswordMutation } from '../generated/graphql';
+import NextLink from 'next/link';
+import {
+  ForgotPasswordInput,
+  useForgotPasswordMutation,
+} from '../generated/graphql';
+import { useCheckAuth } from '../utils/useCheckAuth';
 
 const ForgotPassword = () => {
+  const { data: authData, loading: authLoading } = useCheckAuth();
   const initialValues = { email: '' };
-  const [forgotPassword, { loading, data }] = useForgotPasswordMutation()
+  const [forgotPassword, { loading, data }] = useForgotPasswordMutation();
 
   const onForgotPasswordSubmit = async (values: ForgotPasswordInput) => {
-    await forgotPassword({ variables: { forgotPasswordInput: values } })
-}
-  return (
-    <Wrapper>
-      <Formik initialValues={initialValues} onSubmit={onForgotPasswordSubmit}>
-        {({ isSubmitting }) =>
-          !loading && data ? (
-            <Box>Please check your inbox</Box>
-          ) : (
-            <Form>
-              <InputField
-                name="email"
-                placeholder="Email"
-                label="Email"
-                type="email"
-              />
+    await forgotPassword({ variables: { forgotPasswordInput: values } });
+  };
+  if (authLoading || (!authLoading && authData?.me)) {
+    return (
+      <Flex justifyContent="center" alignItems="center" minH="100vh">
+        <Spinner />
+      </Flex>
+    );
+  } else {
+    return (
+      <Wrapper>
+        <Formik initialValues={initialValues} onSubmit={onForgotPasswordSubmit}>
+          {({ isSubmitting }) =>
+            !loading && data ? (
+              <Box>Please check your inbox</Box>
+            ) : (
+              <Form>
+                <InputField
+                  name="email"
+                  placeholder="Email"
+                  label="Email"
+                  type="email"
+                />
 
-              <Flex mt={2}>
-                <NextLink href="/login">
-                  <Link ml="auto">Back to Login</Link>
-                </NextLink>
-              </Flex>
+                <Flex mt={2}>
+                  <NextLink href="/login">
+                    <Link ml="auto">Back to Login</Link>
+                  </NextLink>
+                </Flex>
 
-              <Button
-                type="submit"
-                colorScheme="teal"
-                mt={4}
-                isLoading={isSubmitting}
-              >
-                Send Reset Password Email
-              </Button>
-            </Form>
-          )
-        }
-      </Formik>
-    </Wrapper>
-  );
+                <Button
+                  type="submit"
+                  colorScheme="teal"
+                  mt={4}
+                  isLoading={isSubmitting}
+                >
+                  Send Reset Password Email
+                </Button>
+              </Form>
+            )
+          }
+        </Formik>
+      </Wrapper>
+    );
+  }
 };
 
 export default ForgotPassword;
