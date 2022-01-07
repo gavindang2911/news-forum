65b7e5ba2d94b53422c73d3ab43fb3adfc27d21f
gavindang2911 <hoaidang2999@gmail.com>
2022-01-07 01:16:18 -0800

    Input field textare field as optional

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,24 +1,35 @@
-import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react'
-import { useField } from 'formik'
-import React from 'react'
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Textarea,
+} from '@chakra-ui/react';
+import { useField } from 'formik';
+import React from 'react';
 
 interface InputFieldProps {
-    name: string
-    label: string
-    placeholder: string
-    type: string
+  name: string;
+  label: string;
+  placeholder: string;
+  type: string;
+  textarea?: boolean;
 }
 
-const InputField = (props: InputFieldProps) => {
-    const [field, {error}] = useField(props)
+const InputField = ({textarea, ...props}: InputFieldProps) => {
+  const [field, { error }] = useField(props);
 
-    return (
-        <FormControl isInvalid={!!error}>
-            <FormLabel htmlFor={props.name}>{props.label}</FormLabel>
-            <Input {...field} id={field.name} {...props}></Input>
-            {error && <FormErrorMessage>{error}</FormErrorMessage>}
-        </FormControl>
-    )
-}
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={props.name}>{props.label}</FormLabel>
+      {textarea ? (
+        <Textarea {...field} id={field.name} {...props} />
+      ) : (
+        <Input {...field} id={field.name} {...props}></Input>
+      )}
+      {error && <FormErrorMessage>{error}</FormErrorMessage>}
+    </FormControl>
+  );
+};
 
-export default InputField
+export default InputField;
