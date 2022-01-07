4aa2fa831a2c57f4d52084d8727120baa517e2a3
gavindang2911 <hoaidang2999@gmail.com>
2022-01-06 21:27:34 -0800

    add timestamptz into specific post

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -26,10 +26,10 @@ export class Post extends BaseEntity {
     text!: string
 
     @Field()
-    @CreateDateColumn()
+    @CreateDateColumn({ type: 'timestamptz' })
     createdAt: Date
 
     @Field()
-    @CreateDateColumn()
+    @CreateDateColumn({ type: 'timestamptz' })
     updatedAt: Date
 }
