ef19668acdc7b90add5a9439d05608c8d154b1b5
gavindang2911 <hoaidang2999@gmail.com>
2022-01-06 21:38:27 -0800

    Added paginated ed post query in back end side

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,12 @@
 import { PaginatedPosts } from '../types/PaginatedPosts';
-import { Arg, FieldResolver, ID, Mutation, Query, Resolver, Root, UseMiddleware } from 'type-graphql';
+import { Arg, FieldResolver, ID, Int, Mutation, Query, Resolver, Root, UseMiddleware } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { User } from '../entities/User';
 import { checkAuth } from '../middleware/checkAuth';
 import { CreatePostInput } from '../types/CreatePostInput';
 import { PostMutationResponse } from '../types/PostMutationResponse';
 import { UpdatePostInput } from '../types/UpdatePostInput';
+import { LessThan } from 'typeorm';
 
 @Resolver(_of => Post)
 export class PostResolver {
@@ -51,14 +52,45 @@ export class PostResolver {
     }
   }
 
-  @Query((_return) => PaginatedPosts, { nullable: true })
-  async posts(): Promise<PaginatedPosts | null> {
-    try {
-      return await Post.find();
-    } catch (error) {
-      return null;
-    }
-  }
+  @Query(_return => PaginatedPosts, { nullable: true })
+	async posts(
+		@Arg('limit', _type => Int) limit: number,
+		@Arg('cursor', { nullable: true }) cursor?: string
+	): Promise<PaginatedPosts | null> {
+		try {
+			const totalPostCount = await Post.count()
+			const realLimit = Math.min(10, limit)
+
+			const findOptions: { [key: string]: any } = {
+				order: {
+					createdAt: 'DESC'
+				},
+				take: realLimit
+			}
+
+			let lastPost: Post[] = []
+			if (cursor) {
+				findOptions.where = { createdAt: LessThan(cursor) }
+
+				lastPost = await Post.find({ order: { createdAt: 'ASC' }, take: 1 })
+			}
+
+			const posts = await Post.find(findOptions)
+
+			return {
+				totalCount: totalPostCount,
+				cursor: posts[posts.length - 1].createdAt,
+				hasMore: cursor
+					? posts[posts.length - 1].createdAt.toString() !==
+					  lastPost[0].createdAt.toString()
+					: posts.length !== totalPostCount,
+				paginatedPosts: posts
+			}
+		} catch (error) {
+			console.log(error)
+			return null
+		}
+	}
 
   @Query((_return) => Post, { nullable: true })
   async post(@Arg('id', (_type) => ID) id: number): Promise<Post | undefined> {
