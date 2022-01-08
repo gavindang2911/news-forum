f0fda774968e0a62aa90439bb14e979fdf7def90
gavindang2911 <hoaidang2999@gmail.com>
2022-01-07 20:37:46 -0800

    Added upvote entities to database

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
 import { COOKIE_NAME, __prod__ } from './constants';
 import { Post } from './entities/Post';
+import { Upvote } from './entities/Upvote';
 import { User } from './entities/User';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
@@ -18,14 +19,14 @@ import { UserResolver } from './resolvers/user';
 import { Context } from './types/Context';
 
 const main = async () => {
-  await createConnection({
+  const connection = await createConnection({
     type: 'postgres',
     database: 'reddit',
     username: process.env.DB_USERNAME_DEV,
     password: process.env.DB_PASSWORD_DEV,
     logging: true,
     synchronize: true,
-    entities: [User, Post],
+    entities: [User, Post, Upvote],
   });
 
 
@@ -73,7 +74,7 @@ const main = async () => {
       resolvers: [HelloResolver, UserResolver, PostResolver],
       validate: false,
     }),
-    context: ({req, res}):Context => ({req, res}),
+    context: ({req, res}):Context => ({req, res, connection}),
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   });
 
